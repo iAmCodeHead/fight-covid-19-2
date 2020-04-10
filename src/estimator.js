483f68b8ced8bd7552915372e992c0182e4c4112
akande samson <akandesamson12@gmail.com>
2020-04-10 21:15:55 +0100

    ......

--- src/estimator.js
+++ src/estimator.js
@@ -35,23 +35,28 @@ const covid19ImpactEstimator = (data) => {
 
   const x1 = resp.impact;
   const y1 = resp.severeImpact;
-  const multiY = (y1.infectionsByRequestedTime * data.region.avgDailyIncomePopulation);
-  const multiX = (x1.infectionsByRequestedTime * data.region.avgDailyIncomePopulation);
+
 
   x1.currentlyInfected = data.reportedCases * 10;
   x1.infectionsByRequestedTime = estimateCurrentlyInfected(x1.currentlyInfected, data.timeToElapse);
   x1.severeCasesByRequestedTime = estimateInfectionsByRequstedTime(x1.infectionsByRequestedTime);
-  x1.hospitalBedsByRequestedTime = c(data.totalHospitalBeds - x1.severeCasesByRequestedTime);
+  x1.hospitalBedsByRequestedTime = c(data.totalHospitalBeds, x1.severeCasesByRequestedTime);
   x1.casesForICUByRequestedTime = x1.infectionsByRequestedTime * 0.05;
   x1.casesForVentilatorsByRequestedTime = x1.infectionsByRequestedTime * 0.02;
+
+  const multiX = (x1.infectionsByRequestedTime * data.region.avgDailyIncomePopulation);
+
   x1.dollarsInFlight = multiX * data.region.avgDailyIncomeInUSD * time(data);
 
   y1.currentlyInfected = data.reportedCases * 50;
   y1.infectionsByRequestedTime = estimateCurrentlyInfected(y1.currentlyInfected, data.timeToElapse);
   y1.severeCasesByRequestedTime = estimateInfectionsByRequstedTime(y1.infectionsByRequestedTime);
-  y1.hospitalBedsByRequestedTime = c(data.totalHospitalBeds - y1.severeCasesByRequestedTime);
+  y1.hospitalBedsByRequestedTime = c(data.totalHospitalBeds, y1.severeCasesByRequestedTime);
   y1.casesForICUByRequestedTime = y1.infectionsByRequestedTime * 0.05;
   y1.casesForVentilatorsByRequestedTime = y1.infectionsByRequestedTime * 0.02;
+
+  const multiY = (y1.infectionsByRequestedTime * data.region.avgDailyIncomePopulation);
+
   y1.dollarsInFlight = multiY * data.region.avgDailyIncomeInUSD * time(data);
 
   return estimated(resp);
